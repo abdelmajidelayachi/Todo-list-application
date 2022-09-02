389b6acec438233f1e0e2dc2bdc965aaed8261a5
El Ayachi Abdelmajid <elayachiabdel2001@gmail.com>
2022-09-02 14:45:02 +0100

    create : interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './Todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todolist';
+  todos : Todo[] = [];
+  newTodo : string;
+  saveTodo(){
+    if(this.newTodo){
+      let todo =  new Todo();
+      todo.name = this.newTodo;
+      todo.isCompleted = true;
+      this.todos.push(todo);
+      this.newTodo='';
+    }else{
+      console.log("please fill the input!!");
+      console.log(this.newTodo);
+      
+    }
+  }
+  editTodo(index:number){
+    this.todos[index].isCompleted=!this.todos[index].isCompleted
+  }
+  deleteList(i:number){
+    this.todos=this.todos.filter((e,index:number)=>index!==i)
+  }
 }
